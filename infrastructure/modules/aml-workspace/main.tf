134e52d658e9d782d2ffc541cd25870288a5b9bd
André Marques Alves <andre.marques.alves@fidelidade.pt>
2022-06-29 15:44:33 +0100

    Update vm size

--- infrastructure/modules/aml-workspace/main.tf
+++ infrastructure/modules/aml-workspace/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_machine_learning_compute_cluster" "adl_aml_ws_compute_cluster"
   name                          = "cpu-cluster"
   location                      = var.location
   vm_priority                   = "LowPriority"
-  vm_size                       = "Standard_DS1_v21"
+  vm_size                       = "Standard_D1"
   machine_learning_workspace_id = azurerm_machine_learning_workspace.mlw.id
   count                         = var.enable_aml_computecluster ? 1 : 0
 
